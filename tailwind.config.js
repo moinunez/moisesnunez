4009528ecd2a5d08158cae3194c453c0a44ca1a7
Moises Nunez <moises5610@gmail.com>
2022-07-08 00:18:43 -0500

    Update tailwind primary colors and tsconfig base url

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: {
+        ...colors.black,
+        primary: '#2C2C34',
+      },
+      blue: {
+        ...colors.blue,
+        primary: '#7189FF',
+        dark: '#3A405A',
+      },
+      brown: {
+        primary: '#C1B4AE',
+      },
+      red: {
+        ...colors.red,
+        primary: '#BC4B51',
+      },
+      white: {
+        ...colors.white,
+        primary: '#EBEBEB',
+      },
+    },
     extend: {},
   },
   plugins: [],
