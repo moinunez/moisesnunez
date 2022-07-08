8a1fcb4bc59bf7bc59c967434d1d1fd44b29d468
Moises Nunez <moises5610@gmail.com>
2022-07-08 00:18:53 -0500

    Update primary colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <header >
-       <h1>Moises Nunez</h1>
+    <div className="h-screen flex items-center justify-center bg-black-primary">
+      <header>
+       <h1 className="text-white-primary">Moises Nunez</h1>
       </header>
     </div>
   );
